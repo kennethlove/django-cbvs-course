412c9ad64479fec5c564a97d0b99b79daed1ba50
Kenneth Love <kennethlove@gmail.com>
2016-06-29 14:47:27 -0700

    first commit

---
+++ djangoal/teams/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+POSITIONS = (
+    ('GK', 'Goalkeeper'),
+    ('CB', 'Center fullback'),
+    ('SW', 'Sweeper'),
+    ('LFB', 'Left fullback'),
+    ('RFB', 'Right fullback'),
+    ('WB', 'Wingback'),
+    ('LM', 'Left midfield'),
+    ('RM', 'Right midfield'),
+    ('DM', 'Defensive midfield'),
+    ('CM', 'Center midfield'),
+    ('WM', 'Wide midfield'),
+    ('CF', 'Center forward'),
+    ('AM', 'Attacking midfield'),
+    ('S', 'Striker'),
+    ('SS', 'Second striker'),
+    ('LW', 'Left winger'),
+    ('RW', 'Right winger'),
+
+)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    coach = models.ForeignKey(User, related_name='teams')
+    practice_location = models.CharField(max_length=255)
+
+    def __string__(self):
+        return self.name
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.PositiveIntegerField()
+    position = models.CharField(choices=POSITIONS, max_length=3)
+    team = models.ForeignKey(Team, related_name='players')
+
+    def __str__(self):
+        return self.name
