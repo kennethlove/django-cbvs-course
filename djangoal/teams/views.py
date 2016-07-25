9fffd3f63653d075bf309aeda0e5ed271fafeca9
Kenneth Love <kennethlove@gmail.com>
2016-07-25 16:44:25 -0700

    delete view is user-restricted, create view automatically uses current user

--- djangoal/teams/views.py
+++ djangoal/teams/views.py
@@ -30,6 +30,11 @@ class TeamCreateView(CreateView):
     fields = ('name', 'practice_location', 'coach')
     model = models.Team
 
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['coach'] = self.request.user.pk
+        return initial
+
 
 class TeamUpdateView(UpdateView):
     fields = ('name', 'practice_location', 'coach')
@@ -38,3 +43,8 @@ class TeamUpdateView(UpdateView):
 
 class TeamDeleteView(DeleteView):
     model = models.Team
+
+    def get_queryset(self):
+        if not self.request.user.is_superuser:
+            return self.model.objects.filter(coach=self.request.user)
+        return self.model.objects.all()
